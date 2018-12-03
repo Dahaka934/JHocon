19ab6dec4f16e0365626b53619a10d3b2eb14b81
Dahaka934 <dahaka934@gmail.com>
2018-12-03 15:24:08 +0300

    Add methods JHocon#toHocon

--- src/main/java/com/github/dahaka934/jhocon/JHocon.java
+++ src/main/java/com/github/dahaka934/jhocon/JHocon.java
@@ -109,6 +109,47 @@ public final class JHocon {
         return toConfig(src, src.getClass());
     }
 
+    /**
+     * Analog of {@link Gson#toJson(Object, Type)}.
+     *
+     * @param src the generic object for which HOCON representation is to be created
+     * @param typeOfSrc the specific genericized type of {@code src}
+     * @param opts the specific render options
+     * @return HOCON representation of {@code src}
+     * @throws JsonIOException if there was a problem writing to the writer
+     */
+    public String toHocon(Object src, Type typeOfSrc, ConfigRenderOptions opts) throws JsonIOException {
+        ConfigValue configValue = toConfigValue(src, typeOfSrc);
+        return renderConfig(configValue, opts);
+    }
+
+    /**
+     * Analog of {@link Gson#toJson(Object)}.
+     *
+     * @param src the non-generic object for which HOCON representation is to be created
+     * @param opts the specific render options
+     * @return HOCON representation of {@code src}
+     * @throws JsonIOException if there was a problem writing to the writer
+     */
+    public String toHocon(Object src, ConfigRenderOptions opts) throws JsonIOException {
+        src = safeObject(src);
+        return toHocon(src, src.getClass(), opts);
+    }
+
+    /**
+     * @see JHocon#toHocon(Object, Type, ConfigRenderOptions)
+     */
+    public String toHocon(Object src, Type typeOfSrc) throws JsonIOException {
+        return toHocon(src, typeOfSrc, null);
+    }
+
+    /**
+     * @see JHocon#toHocon(Object, Type, ConfigRenderOptions)
+     */
+    public String toHocon(Object src) throws JsonIOException {
+        return toHocon(src, (ConfigRenderOptions) null);
+    }
+
     private static Object safeObject(Object obj) {
         return obj != null ? obj : JsonNull.INSTANCE;
     }
