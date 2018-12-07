60d141f401c3c194c07fc71f021d740587baa4d2
Dahaka934 <dahaka934@gmail.com>
2018-12-07 19:01:06 +0300

    Add common method for convert object to string

--- src/main/java/com/github/dahaka934/jhocon/JHoconHelper.java
+++ src/main/java/com/github/dahaka934/jhocon/JHoconHelper.java
@@ -7,6 +7,8 @@ import com.google.gson.stream.JsonWriter;
 import com.typesafe.config.ConfigRenderOptions;
 import com.typesafe.config.ConfigValue;
 
+import java.util.Arrays;
+
 public final class JHoconHelper {
     private JHoconHelper() {
         throw new UnsupportedOperationException();
@@ -41,6 +43,35 @@ public final class JHoconHelper {
     public static void comment(JsonWriter writer, String comment) {
         if (writer instanceof JHoconWriter) {
             ((JHoconWriter) writer).comment(comment);
+        }
+    }
+
+    /**
+     * Convert object to string.
+     */
+    public static String toString(Object obj) {
+        if (obj == null) {
+            return "(null)";
+        } else if (!obj.getClass().isArray()) {
+            return obj.toString();
+        } else if (obj instanceof Object[]) {
+            return Arrays.toString((Object[]) obj);
+        } else if (obj instanceof boolean[]) {
+            return Arrays.toString((boolean[]) obj);
+        } else if (obj instanceof byte[]) {
+            return Arrays.toString((byte[]) obj);
+        } else if (obj instanceof char[]) {
+            return Arrays.toString((char[]) obj);
+        } else if (obj instanceof short[]) {
+            return Arrays.toString((short[]) obj);
+        } else if (obj instanceof int[]) {
+            return Arrays.toString((int[]) obj);
+        } else if (obj instanceof float[]) {
+            return Arrays.toString((float[]) obj);
+        } else if (obj instanceof double[]) {
+            return Arrays.toString((double[]) obj);
+        } else {
+            return "";
         }
     }
 }
