52d51d36426947fd12ad714bd1bcee6c3ceb4324
Dahaka934 <dahaka934@gmail.com>
2018-12-07 20:25:27 +0300

    Add validator to initBuilder

--- src/main/java/com/github/dahaka934/jhocon/JHoconHelper.java
+++ src/main/java/com/github/dahaka934/jhocon/JHoconHelper.java
@@ -2,6 +2,7 @@ package com.github.dahaka934.jhocon;
 
 import com.github.dahaka934.jhocon.fieldlhandler.FieldHandlerComment;
 import com.github.dahaka934.jhocon.fieldlhandler.FieldHandlerCommentDefaultValue;
+import com.github.dahaka934.jhocon.fieldlhandler.FieldHandlerValidator;
 import com.github.dahaka934.jhocon.writer.JHoconWriter;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonSyntaxException;
@@ -20,9 +21,13 @@ public final class JHoconHelper {
      * Register JHocon features in GsonBuilder.
      */
     public static GsonBuilder initBuilder(GsonBuilder builder) {
+        FieldHandlerValidator validator = new FieldHandlerValidator(false);
+
         JHReflectTypeAdapterFactory factory = new JHReflectTypeAdapterFactory();
         factory.register(new FieldHandlerComment());
         factory.register(new FieldHandlerCommentDefaultValue());
+        factory.register(validator);
+
         builder.registerTypeAdapterFactory(factory);
         return builder;
     }
