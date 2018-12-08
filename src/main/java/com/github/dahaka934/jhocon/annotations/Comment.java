d5533fe33d527b6b0b73f1abf26267a20821fe90
Dahaka934 <dahaka934@gmail.com>
2018-12-08 15:42:14 +0300

    Update java-doc

--- src/main/java/com/github/dahaka934/jhocon/annotations/Comment.java
+++ src/main/java/com/github/dahaka934/jhocon/annotations/Comment.java
@@ -1,13 +1,18 @@
 package com.github.dahaka934.jhocon.annotations;
 
 import com.github.dahaka934.jhocon.JHReflectTypeAdapterFactory;
+import com.github.dahaka934.jhocon.fieldlhandler.FieldHandler;
+import com.github.dahaka934.jhocon.fieldlhandler.FieldHandlerComment;
 import com.google.gson.GsonBuilder;
 
 import java.lang.annotation.*;
 
 /**
  * This is annotation used for setting a comment above the field.<br/>
- * Used only if {@link JHReflectTypeAdapterFactory} is registered in {@link GsonBuilder}.<br/>
+ * You must register a {@link JHReflectTypeAdapterFactory} in {@link GsonBuilder}.<br/>
+ * You must register implementer ({@link FieldHandler}) of this annotation in {@link JHReflectTypeAdapterFactory}.<br/>
+ * Default implementer is {@link FieldHandlerComment}.<br/>
+ * <br/>
  * You can use '$value' for inline default field value to comment line.<br/>
  */
 @Documented
--- src/main/java/com/github/dahaka934/jhocon/annotations/ValidatorCustom.java
+++ src/main/java/com/github/dahaka934/jhocon/annotations/ValidatorCustom.java
@@ -1,14 +1,19 @@
 package com.github.dahaka934.jhocon.annotations;
 
 import com.github.dahaka934.jhocon.JHReflectTypeAdapterFactory;
+import com.github.dahaka934.jhocon.fieldlhandler.FieldHandlerValidator;
 import com.github.dahaka934.jhocon.fieldlhandler.validator.FieldValidator;
+import com.github.dahaka934.jhocon.fieldlhandler.validator.FieldValidatorCustomAnnotation;
 import com.google.gson.GsonBuilder;
 
 import java.lang.annotation.*;
 
 /**
  * This is annotation used for setting custom {@link FieldValidator} for field.<br/>
- * Used only if {@link JHReflectTypeAdapterFactory} is registered in {@link GsonBuilder}.<br/>
+ * You must register a {@link JHReflectTypeAdapterFactory} in {@link GsonBuilder}.<br/>
+ * You must register a {@link FieldHandlerValidator} in {@link JHReflectTypeAdapterFactory}.<br/>
+ * You must register implementer ({@link FieldValidator}) of this annotation in {@link FieldHandlerValidator}.<br/>
+ * Default implementer is {@link FieldValidatorCustomAnnotation}.<br/>
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
