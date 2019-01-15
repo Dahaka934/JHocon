72c23f32088de8d53fb4d349e1e72c178c6f26a3
Dahaka934 <dahaka934@gmail.com>
2019-01-15 22:17:05 +0300

    Update JsonReaderStub.java

--- src/main/java/com/google/gson/stream/JsonReaderStub.java
+++ src/main/java/com/google/gson/stream/JsonReaderStub.java
@@ -1,7 +1,10 @@
 package com.google.gson.stream;
 
+import com.google.gson.internal.reflect.ReflectionAccessor;
+
 import java.io.IOException;
 import java.io.Reader;
+import java.lang.reflect.Method;
 
 public class JsonReaderStub extends JsonReader {
     private static final Reader STUB_READER = new Reader() {
@@ -13,6 +16,15 @@ public class JsonReaderStub extends JsonReader {
         @Override
         public void close() throws IOException { }
     };
+
+    static {
+        try {
+            Method m = JsonReaderStub.class.getDeclaredMethod("doPeek");
+            ReflectionAccessor.getInstance().makeAccessible(m);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
 
     public JsonReaderStub() {
         super(STUB_READER);
